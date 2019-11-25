33ba341240ff110a917e97f598ff09a0d35e2a95
Adiatma Kamarudin <adiatma.kamaarudin@ralali.com>
2019-11-25 15:09:54 +0700

    feat: added graphiql

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/adiatma/moviedb-golang-graphql
 go 1.13
 
 require (
-	github.com/friendsofgo/graphiql v0.2.2 // indirect
+	github.com/friendsofgo/graphiql v0.2.2
 	github.com/graphql-go/graphql v0.7.8
 	github.com/joho/godotenv v1.3.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/graphql-go/graphql"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+
+	"github.com/friendsofgo/graphiql"
+	"github.com/graphql-go/graphql"
 )
 
 func main() {
@@ -16,16 +21,49 @@ func main() {
 			},
 		},
 	}
+
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+
 	schema, _ := graphql.NewSchema(schemaConfig)
 
-	query := `{ hello }`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation %+v", r.Errors)
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+
+		if err != nil {
+			log.Fatalln("Error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		var apolloQuery map[string]interface{}
+
+		json.Unmarshal(body, &apolloQuery)
+
+		query := apolloQuery["query"]
+
+		params := graphql.Params{
+			Schema:        schema,
+			RequestString: query.(string),
+		}
+
+		result := graphql.Do(params)
+
+		if len(result.Errors) > 0 {
+			fmt.Printf("Error %+v", result.Errors)
+		}
+
+		json.NewEncoder(w).Encode(result)
+	})
+
+	graphiqlHandler, err := graphiql.NewGraphiqlHandler("/graphql")
+
+	if err != nil {
+		panic(err)
 	}
-	rJSON, _ := json.Marshal(r)
-	fmt.Printf("%s \n", rJSON)
+
+	http.Handle("/graphiql", graphiqlHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
