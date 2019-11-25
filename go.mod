051f92c91939e3697dd140ad1400a481f22d4146
Adiatma Kamarudin <adiatma.kamaarudin@ralali.com>
2019-11-25 11:44:49 +0700

    add: graphql in src/main.go

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/adiatma/moviedb-golang-graphql
 go 1.13
 
 require (
-	github.com/graphql-go/graphql v0.7.8 // indirect
+	github.com/graphql-go/graphql v0.7.8
 	github.com/joho/godotenv v1.3.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+import "github.com/graphql-go/graphql"
+
+import "log"
+
+import "encoding/json"
+
 func main() {
-	fmt.Println("Hello")
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, _ := graphql.NewSchema(schemaConfig)
+
+	query := `{ hello }`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 }
